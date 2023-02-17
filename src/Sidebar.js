4649d2fbd8dff08c0feeb802fc3e63e15e6273cc
Fiona Attah <princessfiona111@gmail.com>
2023-02-17 02:39:53 -0500

    new changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt=""/>
+        <img src="https://images.unsplash.com/photo-1568346744179-a29f1a59cc35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt=""/>
         <Avatar className="sidebar__avatar" />
         <h2>Lisa Johnson</h2>
         <h4>lisajohnson@gmail.com</h4>
